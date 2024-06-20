a410eb44b61a12a00190c9ae605c3c98a6f98ca8
patrick muthii <murimipatrick101@gmail.com>
2024-06-20 05:51:48 -0700

    new changes

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,13 +1,12 @@
- number1 = 10
- number2 = 5
+number1 = 10
+number2 = 5
  
- sum = ("number1 + number2")
+sum_result = number1 + number2
+product_result = number1 * number2
+difference_result = number1 - number2
 
- product = ("number1 * number2")
-  
-  diffrence = ("number1 - number2")
 
-  print(sum)
-  print(product)
- print(diffrence)
+print(sum_result)
+print(product_result)
+print(difference_result)
 
