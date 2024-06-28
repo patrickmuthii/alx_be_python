ee6f99a9bc89792951b0cdf78220bdecb1237dd3
patrickmuthii <147545021+patrickmuthii@users.noreply.github.com>
2024-06-28 18:46:40 +0300

    Update match_case_calculator.py

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -3,4 +3,26 @@ num2 = float(input("Enter the second number"))
 operation = input("Choose the operation (+, -, *, /)")
 
 match case:
-case:
+    case:
+      "+"
+      results = num1 + num2
+      print("The result is: ", result)
+    case:
+      "-"
+      result = num1 - num2
+      print("The result is: ", result)
+    case:
+      "/"
+      result = num1 / num2
+      print("The result is: ", result)
+      if num1/0: print("can't devide by zero")
+    case:
+      "*"
+      result =num1 * num2 
+      print("The result is: ", result)
+    
+
+      
+      
+      
+
