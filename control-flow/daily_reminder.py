a7e54b32444be2998843eeb7c6f0ea1170868cea
patrick muthii <murimipatrick101@gmail.com>
2024-06-30 02:11:27 -0700

    made new changes

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,21 +1,21 @@
 task = input("input task description")
-priority = input("task priority(high, low, medium")
-time_bound = input("Is the task time bound(yes/no)")
+priority = str(input("task priority(high, low, medium"))
+time_bound = str(input("Is the task time bound(yes/no)"))
 
 reminder = ""
         
 match priority:
     case "high":
-        remeinder = "The task is of high priority"
+        reminder = "The task is of high priority"
     case "medium":
         remeinder = "The task is of medium priority"
     case "low":
-        reminder = "The task is of low priority"
-    case_:
+        remeinder = "The task is of low priority"
+    case _:
         reminder = "Invalid priority level entered"
 
 if time_bound == "yes":
-    remeinder =+ "that requires immediate attention today!"
+    reminder += "that requires immediate attention today!"
 
 
 print(reminder)    
