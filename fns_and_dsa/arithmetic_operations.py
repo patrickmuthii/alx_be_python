7fb6c0f7a320efef4d3061c175d93369c0a9e01c
patrick muthii <murimipatrick101@gmail.com>
2024-07-07 03:56:01 -0700

    new correction

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -5,7 +5,7 @@ def perform_operation(num1, num2,operation):
         return num1 - num2
     elif operation == "multiply":
         return num1 * num2
-    elif operation "divide":
+    elif operation == "divide":
         if num2 == 0:
             return "division_by_zero_ERROR"
         return num1 / num2
