2fa5b71ed4c6f5669a0b9dd32b37b53a7d3e4890
patrick muthii <murimipatrick101@gmail.com>
2024-07-07 03:28:04 -0700

    added new script arithmetic.py

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,18 @@
+def perform_operation(float(nun1, num2,str(operation))):
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtraction":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation "divide":
+        if num2 == 0:
+            return "division_by_zero_ERROR"
+        return num1 / num2
+    else:
+        return "invalid operation"
+        
+
+
+
+
