946cdb3f8d2bcd07e3675a24b8e027a79c61b6bf
patrick muthii <murimipatrick101@gmail.com>
2024-07-07 05:26:16 -0700

    new insertions

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -5,9 +5,9 @@ def display_menu():
     print("3. View list")
     print("4. Exit")
 
+
 def main():
     shopping_list = []
-
 
     while True:
         display_menu()
@@ -16,25 +16,24 @@ def main():
         if choice == '1':
             item = input('Enter the item to add: ')
             shopping_list.append(item)
-            print(f'Item {item} added')
+            print(f'Item "{item}" added')
         elif choice == '2':
             item = input('Enter the item to remove: ')
             if item in shopping_list:
                 shopping_list.remove(item)
-            print(f'Item {item} removed')
-        else:
-            print(f'Item {item} not found in the shopping list')
-
+                print(f'Item "{item}" removed')
+            else:
+                print(f'Item "{item}" not found in the shopping list')
         elif choice == '3':
+            print("Shopping List:")
             for item in shopping_list:
-            print(item)
+                print(item)
         elif choice == '4':
             print('Goodbye!')
             break
         else:
             print('Invalid choice. Please try again.')
 
-
 if __name__ == "__main__":
-    main()
+    main()    
 
