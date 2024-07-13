b9b9bd54d989e3701ac213370e044f3d9860bc04
patrick muthii <murimipatrick101@gmail.com>
2024-07-13 04:51:34 -0700

    added bank account.py

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,23 @@
+class BankAccount:
+    def __init__ (self, initial_balance = 0):
+        self._account_balance = initial_balance 
+
+    
+    def deposit(self, amount):
+        if deposit > 0:
+            self._account_balance += amount
+        else:
+            print(f'Amount must be positive number')
+        amount + account_balance
+    def withdraw(self, amount):
+        if amount > 0 and amount <= self._account_balance:
+            self._account_balance -= amount
+            return True
+        else:
+            print(f'Insufficient fund in your account')
+    def display_balance(self):
+        print(f'Your account balance is {self._account_balance}')
+
+
+
+
