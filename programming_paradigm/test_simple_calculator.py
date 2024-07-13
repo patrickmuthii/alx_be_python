e2e2f0372dbeb4b5e6fb99869fbbdd06c707b2d7
patrick muthii <murimipatrick101@gmail.com>
2024-07-13 09:58:00 -0700

    new changes

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -24,7 +24,7 @@ class SimpleCalculator:
 
 
 import unittest
-from test_simple_calculator import SimpleCalculator
+from simple_calculator import SimpleCalculator
 
 class TestSimpleCalculator(unittest.TestCase):
     
