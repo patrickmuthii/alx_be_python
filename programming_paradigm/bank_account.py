233ef95c9b398c3d32da849b25c02a2004f59e7f
patrick muthii <murimipatrick101@gmail.com>
2024-07-13 06:00:43 -0700

    new changes

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -5,8 +5,7 @@ class BankAccount:
     
     def deposit(self, amount):
         if amount > 0:
-            self._account_balance += amount
-            print('Deposited: ${amount.2f}')
+            self._account_balance += amoun
         else:
             print(f'Amount must be positive number')
     def withdraw(self, amount):
