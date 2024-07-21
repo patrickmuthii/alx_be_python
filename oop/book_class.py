1a573ba0e1a727641bf5869a2b34cc354f8c0957
patrick muthii <murimipatrick101@gmail.com>
2024-07-21 02:16:00 -0700

    new changes

--- oop/book_class.py
+++ oop/book_class.py
@@ -7,7 +7,7 @@ class Book:
     def __del__(self):
         print(f"Deleting {self.title}")
     def __str__(self):
-        return (f"(title) by (author), published in (year)")
+        return (f"{title} by {author}, published in {year}")
     def __repr__(self):
         return f"Book('{self.title}', '{self.author}', {self.year})"
 
