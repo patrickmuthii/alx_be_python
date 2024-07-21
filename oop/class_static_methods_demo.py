65616f17b19f526cdbee2c173a109c07d529215f
patrick muthii <murimipatrick101@gmail.com>
2024-07-21 03:40:39 -0700

    added new file

---
+++ oop/class_static_methods_demo.py
@@ -0,0 +1,12 @@
+class Calculator:
+    calculation_type = 'Arithmetic Operations'
+
+    @staticmethod
+    def add(a, b):
+
+        """Retuns the sum of two numbers"""
+        return a + b
+    @classmethod
+    def multiply(cls, a, b):
+         print(f"Calculation type: {cls.calculation_type}")
+        return a * b
